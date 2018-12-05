aaa8ec52f2f1ad8fbb0acf5ff9654ea45186d8e3
caoyb2018 <42482918+caoyb2018@users.noreply.github.com>
2018-12-05 17:19:12 +0800

    实现了并集，交集，差集，子集方法

--- js/Set.js
+++ js/Set.js
@@ -1,12 +1,12 @@
 class Set {
-    constructor() {
+    constructor() {//set在js中的实质上就是一个对象
         this.items = {}
     }
-    has(value) {
+    has(value) {//hasOwnProperty() 方法会返回一个布尔值，指示对象自身属性中是否具有指定的属性
         return this.items.hasOwnProperty(value)
     }
     add(value) {
-        if (!this.has(value)) {
+        if (!this.has(value)) {//如果集合中没有这一项的话就添加。set中的项不重复
             this.items[value] = value
             return true
         }     
@@ -14,17 +14,48 @@ class Set {
     }
     remove(value) {
         if (this.has(value)) {
-            delete this.items[value]
+            delete this.items[value]//delete 操作符用于删除对象的某个属性；如果没有指向这个属性的引用，那它最终会被释放。
             return true
         }
         return false
     }
     get size() {
-        return Object.keys(this.items).length
+        return Object.keys(this.items).length//Object.keys() 方法会返回一个由一个给定对象的自身可枚举属性组成的数组，数组中属性名的排列顺序和使用 for...in 循环遍历该对象时返回的顺序一致 。
     }
     get values() {
         return Object.keys(this.items)
     }
+    union(otherSet) {// Set 类实现一个并集方法
+    	const unionSet = new Set()
+    	this.values.forEach((v, i) => unionSet.add(this.values[i]))
+    	otherSet.values.forEach((v, i) => unionSet.add(otherSet.values[i]))
+    	return unionSet
+	}
+    intersection(otherSet) {//Set 类实现一个交集方法
+	    const intersectionSet = new Set()
+	    this.values.forEach((v, i) => {
+        	if (otherSet.has(v)) {
+            	intersectionSet.add(v)
+        	}
+    	})
+    	return intersectionSet
+	}
+    difference(otherSet) {//Set 类实现一个差集方法
+    	const differenceSet = new Set()
+    	this.values.forEach((v, i) => {
+        	if (!otherSet.has(v)) {
+            	differenceSet.add(v)
+        	}
+    	})
+    	return differenceSet
+	}
+    subset(otherSet) {// Set 类实现一个子集方法
+    	if (this.size > otherSet.size) {
+        	return false
+    	} else {
+        	return !this.values.some(v => !otherSet.has(v))
+    	} 
+	}
 }
 const set = new Set()
 set.add(1)
