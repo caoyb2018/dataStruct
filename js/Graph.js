7296b9f9c2ef1b4fcadc674605ac51d5c84a6689
caoyb2018 <caoyb2018@163.com>
2019-01-07 09:33:00 +0800

    增加了广度优先遍历算法

--- js/Graph.js
+++ js/Graph.js
@@ -10,7 +10,6 @@ class Graph{
         this.vertices.push(v)
         this.adjList.set(v, [])
     }
-
     // 添加线
     addEdge(v, w) {
         this.adjList.get(v).push(w)
@@ -24,9 +23,69 @@ class Graph{
             }, `${r}\n${v} => `)
         }, '')
     }
+    // bfs(v, callback) {//广度优先遍历
+    //     const read = []
+    //     const adjList = this.adjList
+    //     let pending = [v || this.vertices[0]]
+    //     const readVertices = vertices => {
+    //         vertices.forEach(key => {
+    //             read.push(key)
+    //             pending.shift()
+    //             adjList.get(key).forEach(v => {
+    //                 if (!pending.includes(v) && !read.includes(v)) {
+    //                     pending.push(v)
+    //                 }
+    //             })
+    //             if (callback) callback(key)
+    //             if (pending.length) readVertices(pending)
+    //         })
+    //     }
+    //     readVertices(pending)
+    // }
+    bfs(v, callback) {//广度优先遍历升级版
+        const read = []
+        const distances = []
+        const predecessors = []
+        const adjList = this.adjList
+        const pending = [v || this.vertices[0]]
+        const readVertices = vertices => {
+            vertices.forEach(key => {
+                read.push(key)
+                pending.shift()
+                distances[key] = distances[key] || 0
+                predecessors[key] = predecessors[key] || null
+                adjList.get(key).forEach(v => {
+                    if (!pending.includes(v) && !read.includes(v)) {
+                        pending.push(v)
+                        distances[v] = distances[key] + 1
+                        predecessors[v] = key
+                    }
+                })
+                if (callback) callback(key)
+                if (pending.length) readVertices(pending)
+            })
+        }
+        readVertices(pending)
+        return { distances, predecessors }
+    }
+    distance(fromVertex) {
+        const vertices = this.vertices
+        const { distances, predecessors } = this.bfs(fromVertex)
+        vertices.forEach(toVertex => {
+            if (!!distances[toVertex]) {
+                let preVertex = predecessors[toVertex]
+                let slug = ''
+                while (fromVertex !== preVertex) {
+                    slug = `${preVertex} - ${slug}`
+                    preVertex = predecessors[preVertex]
+                }
+                slug = `${fromVertex} - ${slug}${toVertex}`
+                console.log(slug)
+            }
+        })
+    }
 }
 const graph = new Graph()
-
 ;['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I'].forEach(c => graph.addVertex(c))
 
 graph.addEdge('A', 'B')
@@ -39,5 +98,6 @@ graph.addEdge('D', 'H')
 graph.addEdge('B', 'E')
 graph.addEdge('B', 'F')
 graph.addEdge('E', 'I')
-
+console.log(graph.bfs(graph.vertices[0], value => console.log('distance: ' + value))) 
+graph.distance(graph.vertices[0])
 console.log(graph.toString())
