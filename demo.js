0840165257371b3b1f4aac02f33bc056286be833
caoyb2018 <caoyb2018@163.com>
2019-12-18 09:37:57 +0800

    Set

--- demo.js
+++ demo.js
@@ -1,16 +1,26 @@
-const CircleLinkedList = require('./js/CircleLinkedList')
-const linkedList = new CircleLinkedList()
+const Set = require('./js/Set')
 
-console.log(linkedList.isEmpty())
-linkedList.append(2)
-linkedList.append(6)
-linkedList.append(24)
-linkedList.append(152)
+const set = new Set();
+const set1 = new Set();
+set.add(1)
+console.log(set.values)  // ["1"] 
+console.log(set.has(1))  // true 
+console.log(set.size) // 1 
+set.add(3) 
+set.add(5)
+set.add(7)
+set.add(9)
+set1.add(2)
+set1.add(4)
+set1.add(6)
+set1.add(7)
+set1.add(8)
 
-linkedList.insert(3, 18)
-linkedList.remove(24)
-console.log(linkedList.size())
-console.log(linkedList.toString())
-console.log(linkedList.findIndex(6))
+console.log(set.difference(set1).values)
+
+console.log(set1.values)
+console.log(set.values)
+
+
 
 
--- js/Set.js
+++ js/Set.js
@@ -1,72 +1,71 @@
 class Set {
-    constructor() {//set在js中的实质上就是一个对象
+    constructor() {
         this.items = {}
     }
-    has(value) {//hasOwnProperty() 方法会返回一个布尔值，指示对象自身属性中是否具有指定的属性
-        return this.items.hasOwnProperty(value)
+
+    has(value) {
+        return this.items.hasOwnProperty(value);
     }
+
     add(value) {
-        if (!this.has(value)) {//如果集合中没有这一项的话就添加。set中的项不重复
-            this.items[value] = value
-            return true
-        }     
-        return false
+        if(!this.has(value)){
+            this.items[value] = value;
+            return true;
+        }else {
+            return false;
+        }
     }
+
+    union(otherSet) {
+        const unionSet = new Set();
+        this.values.forEach((v, i) => unionSet.add(this.values[i]))
+        otherSet.values.forEach((v, i) => unionSet.add(otherSet.values[i]))
+        return unionSet
+    }
+
+    intersection(otherSet) {
+        const intersectionSet = new Set()
+        this.values.forEach((v, i) => {
+            if(otherSet.has(v)){
+                intersectionSet.add(v)
+            }
+        })
+        return intersectionSet
+    }
+
+    difference(otherSet) {
+        const differenceSet = new Set()
+        this.values.forEach((v, i) => {
+            if(!otherSet.has(v)){
+                differenceSet.add(v)
+            }
+        })
+        return differenceSet
+    }
+
+    subset(otherSet) {
+        if(this.size < otherSet.size) {
+            return false;
+        } else {
+            return !this.values.some(v => !otherSet.has(v))
+        } 
+    }
+
     remove(value) {
-        if (this.has(value)) {
-            delete this.items[value]//delete 操作符用于删除对象的某个属性；如果没有指向这个属性的引用，那它最终会被释放。
+        if(this.has(value)) {
+            delete this.items[value]
             return true
         }
-        return false
+        return false;
     }
-    get size() {
-        return Object.keys(this.items).length//Object.keys() 方法会返回一个由一个给定对象的自身可枚举属性组成的数组，数组中属性名的排列顺序和使用 for...in 循环遍历该对象时返回的顺序一致 。
+
+    get size(){
+        return Object.keys(this.items).length
     }
+
     get values() {
         return Object.keys(this.items)
     }
-    union(otherSet) {// Set 类实现一个并集方法
-    	const unionSet = new Set()
-    	this.values.forEach((v, i) => unionSet.add(this.values[i]))
-    	otherSet.values.forEach((v, i) => unionSet.add(otherSet.values[i]))
-    	return unionSet
-	}
-    intersection(otherSet) {//Set 类实现一个交集方法
-	    const intersectionSet = new Set()
-	    this.values.forEach((v, i) => {
-        	if (otherSet.has(v)) {
-            	intersectionSet.add(v)
-        	}
-    	})
-    	return intersectionSet
-	}
-    difference(otherSet) {//Set 类实现一个差集方法
-    	const differenceSet = new Set()
-    	this.values.forEach((v, i) => {
-        	if (!otherSet.has(v)) {
-            	differenceSet.add(v)
-        	}
-    	})
-    	return differenceSet
-	}
-    subset(otherSet) {// Set 类实现一个子集方法
-    	if (this.size > otherSet.size) {
-        	return false
-    	} else {
-        	return !this.values.some(v => !otherSet.has(v))
-    	} 
-	}
 }
-const set = new Set()
-set.add(1)
-console.log(set.values)  // ["1"] 
-console.log(set.has(1))  // true 
-console.log(set.size) // 1 
-set.add(2) 
-console.log(set.values)  // ["1", "2"] 
-console.log(set.has(2))  // true 
-console.log(set.size) // 2 
-set.remove(1) 
-console.log(set.values) // ["2"] 
-set.remove(2) 
-console.log(set.values) // []
+
+module.exports = Set;
